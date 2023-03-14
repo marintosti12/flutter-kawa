d2d4ec7b91aad1f16cd1bdbf88f606bc1b318ec1
Pendi85 <81863481+Pendi85@users.noreply.github.com>
2023-03-15 00:25:14 +0100

    Merge on workflows branch

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -49,7 +49,7 @@ ET contient un champ d'entrée pour saisir une adresse mail
       await tester.pumpWidget(homePage);
 
       //QUAND on appuye sur le bouton "Inscription utilisateur"
-      final inscriptionButton = find.widgetWithText(TextButton, 'Inscription utilisateurTESTCI');
+      final inscriptionButton = find.widgetWithText(TextButton, 'Inscription utilisateur');
       await tester.tap(inscriptionButton);
 
       //ALORS une page permettant de saisir une adresse mail s'affiche
